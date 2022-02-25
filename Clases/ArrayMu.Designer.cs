bed3457440fd1dc27e48ca35c5ed8706ead9107e
Geerux <camancito1@hotmail.com>
2022-02-25 09:52:00 -0700

    ArrayChanges

--- Clases/ArrayMu.Designer.cs
+++ Clases/ArrayMu.Designer.cs
@@ -1,7 +1,11 @@
-﻿namespace Practicasfull
+﻿
+namespace Practicasfull
 {
     partial class ArrayMu
     {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
@@ -32,6 +36,7 @@
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnmostrar = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
@@ -40,42 +45,39 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(130, 98);
+            this.label1.Location = new System.Drawing.Point(76, 77);
             this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(42, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "Valores";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // txtvalor
             // 
-            this.txtvalor.Location = new System.Drawing.Point(200, 98);
+            this.txtvalor.Location = new System.Drawing.Point(127, 75);
             this.txtvalor.Margin = new System.Windows.Forms.Padding(2);
             this.txtvalor.Name = "txtvalor";
-            this.txtvalor.Size = new System.Drawing.Size(72, 20);
+            this.txtvalor.Size = new System.Drawing.Size(82, 20);
             this.txtvalor.TabIndex = 1;
-            this.txtvalor.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // txttamaño
             // 
-            this.txttamaño.Location = new System.Drawing.Point(200, 69);
+            this.txttamaño.Location = new System.Drawing.Point(127, 40);
             this.txttamaño.Margin = new System.Windows.Forms.Padding(2);
             this.txttamaño.Name = "txttamaño";
-            this.txttamaño.Size = new System.Drawing.Size(72, 20);
-            this.txttamaño.TabIndex = 2;
-            this.txttamaño.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            this.txttamaño.Size = new System.Drawing.Size(82, 20);
+            this.txttamaño.TabIndex = 3;
+            this.txttamaño.TextChanged += new System.EventHandler(this.txttamaño_TextChanged);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(132, 69);
+            this.label2.Location = new System.Drawing.Point(76, 42);
             this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(46, 13);
-            this.label2.TabIndex = 3;
+            this.label2.TabIndex = 2;
             this.label2.Text = "Tamaño";
-            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // dataGridView1
             // 
@@ -83,47 +85,58 @@
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Column1,
             this.Column2});
-            this.dataGridView1.Location = new System.Drawing.Point(22, 177);
+            this.dataGridView1.Location = new System.Drawing.Point(79, 114);
             this.dataGridView1.Margin = new System.Windows.Forms.Padding(2);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 82;
-            this.dataGridView1.RowTemplate.Height = 33;
-            this.dataGridView1.Size = new System.Drawing.Size(487, 119);
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(297, 133);
             this.dataGridView1.TabIndex = 4;
             // 
             // Column1
             // 
             this.Column1.HeaderText = "Column1";
-            this.Column1.MinimumWidth = 10;
+            this.Column1.MinimumWidth = 8;
             this.Column1.Name = "Column1";
-            this.Column1.Width = 200;
+            this.Column1.Width = 150;
             // 
             // Column2
             // 
             this.Column2.HeaderText = "Column2";
-            this.Column2.MinimumWidth = 10;
+            this.Column2.MinimumWidth = 8;
             this.Column2.Name = "Column2";
-            this.Column2.Width = 200;
+            this.Column2.Width = 150;
+            // 
+            // btnmostrar
+            // 
+            this.btnmostrar.Location = new System.Drawing.Point(310, 70);
+            this.btnmostrar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnmostrar.Name = "btnmostrar";
+            this.btnmostrar.Size = new System.Drawing.Size(66, 26);
+            this.btnmostrar.TabIndex = 5;
+            this.btnmostrar.Text = "Mostrar";
+            this.btnmostrar.UseVisualStyleBackColor = true;
+            this.btnmostrar.Click += new System.EventHandler(this.btnmostrar_Click);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(293, 98);
+            this.button1.Location = new System.Drawing.Point(221, 70);
             this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(63, 21);
-            this.button1.TabIndex = 5;
-            this.button1.Text = "Mostrar";
+            this.button1.Size = new System.Drawing.Size(66, 26);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Agregar";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(200, 128);
+            this.button2.Location = new System.Drawing.Point(391, 70);
             this.button2.Margin = new System.Windows.Forms.Padding(2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(63, 23);
-            this.button2.TabIndex = 6;
-            this.button2.Text = "Agregar ";
+            this.button2.Size = new System.Drawing.Size(66, 26);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Limpiar";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
@@ -131,18 +144,20 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(520, 359);
+            this.ClientSize = new System.Drawing.Size(598, 345);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnmostrar);
             this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.txttamaño);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.txtvalor);
             this.Controls.Add(this.label1);
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "ArrayMu";
-            this.Text = "arreglobidimensionales";
-            this.Load += new System.EventHandler(this.arreglobidimensionales_Load);
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Arreglos Bidimensionales";
+            this.Load += new System.EventHandler(this.arreglosbidimencionales_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -158,6 +173,7 @@
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.Button btnmostrar;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
     }
---
+++ Clases/ArrayMu.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practicasfull
+{
+    public partial class ArrayMu : Form
+    {
+        public ArrayMu()
+        {
+            InitializeComponent();
+        }
+        int r; 
+        int C = 0; 
+        int contador;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            contador++; 
+            r++; 
+            if (contador > (tamaño * 2)) MessageBox.Show("Lleno");
+            else
+            {
+
+
+
+                if (r > tamaño)
+                {
+                    r = 1;
+                    C = 1; 
+                }
+                if (r <= tamaño)
+                    arreglo[r - 1, C] = txtvalor.Text;
+
+            }
+        }
+        int tamaño = 0;
+        string[,] arreglo;
+        private void txttamaño_TextChanged(object sender, EventArgs e)
+        {
+
+            tamaño = int.Parse(txttamaño.Text);
+            arreglo = new string[tamaño, 2];
+        }
+
+        private void btnmostrar_Click(object sender, EventArgs e)
+        {
+          
+            C = 0; 
+            int r = 0; 
+           
+            DataGridViewRow row; 
+            for (int rr = 0; rr < tamaño; rr++)
+            
+            {
+                row = new DataGridViewRow();
+                row.CreateCells(this.dataGridView1);
+                for (int CC = 0; CC < 2; CC++)
+            
+                {
+                    row.Cells[CC].Value = arreglo[rr, CC];
+
+                    C++;
+                }
+
+                this.dataGridView1.Rows.Add(row);
+                C = 0;
+                r++;
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            contador = 0;
+            Array.Clear(arreglo, 0, arreglo.Length);
+            dataGridView1.Rows.Clear();
+        }
+
+        private void arreglosbidimencionales_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
